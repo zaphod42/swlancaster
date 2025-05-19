fc1d13187b4b48abd0dce5529c5d1b4223ef2302
Andrew Parker <aparker42@gmail.com>
2025-05-19 11:01:10 +0100

    Start connecting to firebase
    I want a login scheme to allow people to say they are going.

--- _site/assets/js/app.mjs
+++ _site/assets/js/app.mjs
@@ -1,4 +1,8 @@
-import getNextMeetupDate from "./schedule.mjs";
+import getNextMeetupDate from './schedule.mjs';
+import { getAuth, connectAuthEmulator } from 'firebase/auth';
+import firebase from 'firebase/compat/app';
+import * as firebaseui from 'firebaseui';
+import 'firebaseui/dist/firebaseui.css';
 
 function showNextMeetingDate(document, console, fetch) {
     document.addEventListener('DOMContentLoaded', function () {
@@ -18,6 +22,33 @@ function showNextMeetingDate(document, console, fetch) {
     });
 }
 
+function connectLoginAction(document) {
+    connectAuthEmulator(getAuth(), 'http://localhost:9099');
+    const ui = new firebaseui.auth.AuthUI(firebase.auth());
+    ui.start(document.getElementById('firebaseui-auth-container'), {
+        signInOptions: [{
+            provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+            signInMethod: firebase.auth.EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD
+        }]
+    });
+}
+
+function initialize() {
+    const firebaseConfig = {
+        apiKey: "AIzaSyBm3vDDHzmIiLFiSJwedNh_J9HS5JIt_2I",
+        authDomain: "software-lancaster.firebaseapp.com",
+        projectId: "software-lancaster",
+        storageBucket: "software-lancaster.firebasestorage.app",
+        messagingSenderId: "844471974310",
+        appId: "1:844471974310:web:bd3450059bf0e49f9b3767"
+    };
+    firebase.initializeApp(firebaseConfig);
+}
+
 export default function run(document, console, fetch) {
     showNextMeetingDate(document, console, fetch);
+    if(document.location.search.match('.*\\bfeature=login\\b.*')) {
+        initialize();
+        connectLoginAction(document);
+    }
 }
